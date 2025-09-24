5b230af4f308e554d4b88757e3de916e07ffaf55
eren5854 <ihsanerendelibas@hotmail.com>
2025-09-24 13:19:31 +0300

    .

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -12,7 +12,8 @@ export class SignalrService {
   startConnection(callback: () => void) {
     this.hub = new signalR.HubConnectionBuilder()
       // .withUrl('https://apismartbuilding.erendelibas.xyz/sensor-hub', {
-      .withUrl('https://192.168.68.134:45455/sensor-hub', {
+      // .withUrl('https://192.168.68.134:45455/sensor-hub', {
+      .withUrl('http://100.127.177.109:54080/sensor-hub', {
         withCredentials: true
       })
       .withAutomaticReconnect()
