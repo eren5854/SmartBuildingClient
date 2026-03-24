87bf2c057f383ccfca840eaeaa3fd381e5b951ea
eren5854 <ihsanerendelibas@hotmail.com>
2026-03-24 05:08:53 +0300

    .

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,10 +17,10 @@ export class LoginComponent {
   loginModel: LoginModel = new LoginModel();
   userModel: UserModel = new UserModel();
 
-  // url:string = "https://apismartbuilding.erendelibas.xyz/api";
+  url:string = "https://apismartbuilding.erendelibas.xyz/api";
   // url:string = "http://188.132.232.172:54080/api";
   // url:string = "http://localhost:54081/api"
-  url:string = "https://localhost:7232/api"
+  // url:string = "https://localhost:7232/api"
   // url:string = "http://100.127.177.109:54080/api";
 
   constructor(
